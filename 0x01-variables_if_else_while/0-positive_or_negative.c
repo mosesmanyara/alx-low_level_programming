e4f58f65c0aede47fe954462a6d163cf753815dc
mosesmanyara <manyaram52@gmail.com>
2022-07-05 17:43:34 -0700

    postneg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,18 +2,19 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main -> assigns a random number to the variable n each time its executed
+ * main - prints a random number to the variable n each time its executed
  * based on conditon
- * Return :  always 0
+ * Return: 0 if excited properly
  */
 int main(void)
-/**
- * main -> assigns a datatpe 
- */
 {
 	int n;
+	/**
+	 * main - prints a datatype
+	 */
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 		printf("%d is postive\n", n);
 	if (n == 0)
