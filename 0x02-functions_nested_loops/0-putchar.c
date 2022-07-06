4b1d81b546f233b5025a8eef76e3d2f6b528ec27
mosesmanyara <manyaram52@gmail.com>
2022-07-05 18:12:02 -0700

    nested

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/** main - prints putchar
+ * Return : 0
+*/
+int main(void)
+{
+	/**
+	 * * main - prints putchar 
+	 * */
+        _purchar("_");
+	_purchar("p");
+	_purchar("u");
+	_purchar("t");
+	_purchar("c");
+	_purchar("a");
+	_purchar("r");
+	_purchar("\n");
+	return (0);
+}
