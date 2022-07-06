8a928be4964a9170d3000c5e2dd2659ad336b80f
mosesmanyara <manyaram52@gmail.com>
2022-07-05 19:15:04 -0700

    corre

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /** main - prints putchar
- * Return : 0
+ * Return : always 0
  */
-
 int main(void)
 {
 	/** main - prints the sizes of various type
 	 * Return: 0 if excited properly
 	 */
 
-        _putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+        _putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
