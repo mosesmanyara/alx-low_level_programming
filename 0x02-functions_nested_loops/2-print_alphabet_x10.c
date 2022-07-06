53d644c6d6adbf4781f87be2d51f2e9dce1dcee0
mosesmanyara <manyaram52@gmail.com>
2022-07-05 20:31:26 -0700

    printsalpahbets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - print lowercase alphabets
- */
+* print_alphabet_x10 - prints the lowercase alphabets
+* Return: Always 0
+*/
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	int j;
 	int x;
-	
+
 	for (x = 0; x < 10; x++)
 	{
 		for (j = 'a' ; j <= 'z' ; j++)
