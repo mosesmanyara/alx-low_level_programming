d946e4ab6b2f149065d3788676fc86e47f1da992
mosesmanyara <manyaram52@gmail.com>
2022-07-05 17:03:05 -0700

    assignnumbern

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -> assigns a random number to the variable n each time its executed
+ * based on conditon
+ * Return :  always 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is postive\n" , n);
+	if ( n== 0)
+		printf("%d is zero\n" , n);
+	if (n < 0)
+		printf("%d is negative\n" , n);
+	return (0);
+}
