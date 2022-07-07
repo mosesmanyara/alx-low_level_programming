8d74f0f128af9fb1ef790fa51df4fa6862ad74ed
mosesmanyara <manyaram52@gmail.com>
2022-07-06 19:12:40 -0700

    baueer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,21 +1,20 @@
 #include "main.h"
 /**
  *jack_bauer - prints 24 hours
- *Return: 0
  */
 void jack_bauer(void)
 {
 	int hour, minute;
-	for (hour = 0; hour <= 23; hour++)
-	{
-		for (minute = 0; minute <= 59; minute++)
+		for (hour = 0; hour <= 23; hour++)
 		{
-			_putchar((hour / 10) + '0');
-			_putchar((hour % 10) + '0');
-			_putchar(':');
-			_putchar((minute / 10) + '0');
-			_putchar((minute % 10) + '0');
-			_putchar('\n');
+			for (minute = 0; minute <= 59; minute++)
+			{
+				_putchar((hour / 10) + '0');
+				_putchar((hour % 10) + '0');
+				_putchar(':');
+				_putchar((minute / 10) + '0');
+				_putchar((minute % 10) + '0');
+				_putchar('\n');
+			}
 		}
-	}
 }
