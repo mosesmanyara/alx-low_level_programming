bad0651febc9cc9940639bf711385510f0b79632
mosesmanyara <manyaram52@gmail.com>
2022-07-06 19:22:08 -0700

    timetable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *times_table - prints 9 times_table
+ */
+int add(int, int)
+{
+	int num, mult, prod;
+		for (num = 0; num <= 9; num++)
+		{
+			_putchar('0');
+			for (mult = 1; mult <= 9; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				prod = num * mult;
+				if (prod <= 9)
+					_putchar(' ');
+				else
+					_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
+		}
+}
