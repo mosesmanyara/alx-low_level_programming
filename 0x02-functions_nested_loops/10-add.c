857c27c1036813ee06056824255daefbcaf813e4
mosesmanyara <manyaram52@gmail.com>
2022-07-12 16:30:01 -0700

    pushnpull

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,13 @@
 #include "main.h"
-/**
- *add - adds two intergers
- *Return: interger
- */
-int add(int a, int b)
-{
-	return (a + b);
-}
 
+/**
+ * add - Adds two integers.
+ * @num1: The first integer to be added.
+ * @num2: The second integer to be added.
+ *
+ * Return: The result of the addition.
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,3 +11,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 int print_last_digit(int);
+int add(int, int);
