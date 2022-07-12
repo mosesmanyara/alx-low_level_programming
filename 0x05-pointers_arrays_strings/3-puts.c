1cab955b53dfdd11618b3dbd7a39a2cd12235030
mosesmanyara <manyaram52@gmail.com>
2022-07-12 15:21:33 -0700

    puutr

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,13 @@
 #include "main.h"
+
 /**
  * _puts - Prints a string to stdout.
  * @str: The string to be printed.
  */
 void _puts(char *str)
 {
-	for (; *str != '\0' ; str++)
-	{
+	while (*str)
 		_putchar(*str++);
-	}
+
 	_putchar('\n');
 }
